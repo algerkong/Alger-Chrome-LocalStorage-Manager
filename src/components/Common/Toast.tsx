5878a739e342aa416e1672fbdb60861f13ff6f43
alger <algerkc@qq.com>
2026-03-11 21:47:48 +0800

    fix: move toast notifications to bottom-right to avoid blocking header

--- src/components/Common/Toast.tsx
+++ src/components/Common/Toast.tsx
@@ -45,7 +45,7 @@ export const ToastProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   return (
     <ToastContext.Provider value={{ addToast }}>
       {children}
-      <div className="fixed top-4 right-4 z-[9999] flex flex-col gap-2 pointer-events-none">
+      <div className="fixed bottom-4 right-4 z-[9999] flex flex-col-reverse gap-2 pointer-events-none">
         {toasts.map(toast => (
           <div
             key={toast.id}
